00432543020593c0ddfaa7cb70e981310d07147b
mandanakhashayar <maks21@student.bth.se>
2022-04-06 14:15:15 +0430

    project initialization

---
+++ src/main/java/com/tdd/tddselldetails/TddSellDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.tdd.tddselldetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddSellDetailsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddSellDetailsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tdd/tddselldetails/TddSellDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdd.tddselldetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddSellDetailsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
